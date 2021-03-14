b5885fe3f5cfc74990d0ccd6b7dbdf3b3f63bfdf
Pypoh <nafifb@programmer.net>
2021-03-14 20:33:53 +0700

    Foodivore Backend Init

---
+++ app/routes/food.routes.js
@@ -0,0 +1,28 @@
+module.exports = app => {
+    const foods = require("../controllers/food.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Create a new Food
+    router.post("/", foods.create);
+  
+    // Retrieve all Food
+    router.get("/", foods.findAll);
+  
+    // Retrieve all published Food
+    router.get("/published", foods.findAllPublished);
+  
+    // Retrieve a single Food with id
+    router.get("/:id", foods.findOne);
+  
+    // Update a Food with id
+    router.put("/:id", foods.update);
+  
+    // Delete a Food with id
+    router.delete("/:id", foods.delete);
+  
+    // Create a new Food
+    router.delete("/", foods.deleteAll);
+  
+    app.use('/api/foods', router);
+  };
