8e29714ed02107e5622aca3974d85ca403759bcb
Pypoh <nafifb@programmer.net>
2021-07-23 07:19:25 +0700

    v0.2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -228,6 +229,8 @@ function initialFoodType() {
 var corsOptions = {
   origin: "http://localhost:8081",
 };
+
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(cors(corsOptions));
 
